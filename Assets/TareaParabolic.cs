f4b9b0aa8afed6ff837aa91910bd1c15a4c0c278
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-03-04 19:46:35 -0600

    inputs

--- Assets/TareaParabolic.cs
+++ Assets/TareaParabolic.cs
@@ -2,13 +2,15 @@
 using UnityEngine.Events;
 using System.Collections.Generic;
 
-public class TareaParabolic: MonoBehaviour
+public class TareaParabolic : MonoBehaviour
 {
 
-    [Tooltip("Position we want to hit")]
     public Vector3 targetPos;
+    
 
-    [Tooltip("Horizontal speed, in units/sec")]
+
+
+[Tooltip("Horizontal speed, in units/sec")]
     public float speed = 10;
 
     [Tooltip("How high the arc should be, in units")]
@@ -20,17 +22,23 @@ public class TareaParabolic: MonoBehaviour
     void Start()
     {
         // guardamos nuestra posicion inicial
-       
+
         startPos = transform.position;
     }
 
     void Update()
     {
         // Calcula la siguiente posicion, con el angulo puesto
+        MovementParabolic();
+    }
+
+    public void MovementParabolic()
+    {
+        // Calcula la siguiente posicion, con el angulo puesto
         float x0 = startPos.x;
         float x1 = targetPos.x;
-        float dist = x1 - x0;
-        float nextX = Mathf.MoveTowards(transform.position.x, x1, speed * Time.deltaTime);
+        float dist = x1 - x0; // sacamos la distancia
+        float nextX = Mathf.MoveTowards(transform.position.x, x1, speed * Time.deltaTime); // El movimiento MRU
         float baseY = Mathf.Lerp(startPos.y, targetPos.y, (nextX - x0) / dist);
         float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist); //la formula para obtener el maximo de  trabajo de arco a cualquier altura de arco que haya especificado.
         Vector3 nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
@@ -38,25 +46,20 @@ public class TareaParabolic: MonoBehaviour
         // Gira para apuntar la siguiente posición y luego muévete allí
         transform.rotation = LookAt2D(nextPos - transform.position);
         transform.position = nextPos;
-        
+
 
         // Haz algo cuando se llega a la posicion
         if (nextPos == targetPos) Arrived();
     }
-
     void Arrived()
     {
         Destroy(gameObject);
     }
 
-    /// 
-    /// This is a 2D version of Quaternion.LookAt; it returns a quaternion
-    /// that makes the local +X axis point in the given forward direction.
-    /// 
-    /// forward direction
-    /// Quaternion that rotates +X to align with forward
+
     static Quaternion LookAt2D(Vector2 forward)
     {
         return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
     }
 }
+
