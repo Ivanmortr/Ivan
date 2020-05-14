9291e64a3d104a686e0b990d158e00fb6a6d4b14
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-05-14 15:37:09 -0500

    Tiro parabolico

---
+++ Assets/tiroparabolico.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiroparabolico : MonoBehaviour
+{
+
+    public Rigidbody pelotita;
+    public int multiplicador;
+    public float KV;
+
+    private void Start()
+    {
+        pelotita = GetComponent<Rigidbody>();
+        multiplicador = 20;
+    }
+
+    public void Update()
+    {
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            KV += Time.deltaTime * multiplicador;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Lanzar();
+        }
+    }
+
+    void Lanzar()
+    {
+        float energiaP = KV;
+        pelotita.AddForce(energiaP * Vector3.up * multiplicador);
+
+    }
+
+}
