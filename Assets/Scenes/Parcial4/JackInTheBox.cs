2e2ffbdf83ff60202f4e11c597a047d43ad27260
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-06-19 22:22:38 -0500

    PracticaOscilacion

--- Assets/Scenes/Parcial4/JackInTheBox.cs
+++ Assets/Scenes/Parcial4/JackInTheBox.cs
@@ -16,18 +16,55 @@ public class JackInTheBox : MonoBehaviour
     public float acc;
 
     public Transform spring;
+
+
+
     public Transform hijo;
     float valor= 0;
     float valor2;
+
+
+    Vector3 minScale;
+    public Vector3 maxScale;
+    public bool repetir;
+    public float speed = 2f;
+    public float duration = 5f;
+
+
+    IEnumerator Start()
+    {
+        minScale = spring.transform.localScale;
+        Test();
+
+        while (repetir)
+        {
+            yield return repetirLerp(minScale, maxScale, duration);
+            yield return repetirLerp(maxScale, minScale, duration);
+        }
+    }
+
     public void Test()
     {
         float displacement = xbalance - a;
         dampening = k * displacement;
         acc += ((k / m) * displacement) - dampening;
         spring.localScale = new Vector3(spring.localScale.x, spring.localScale.y - acc, spring.localScale.z);
-        hijo.localPosition = new Vector3(hijo.localPosition.x,hijo.localPosition.y - acc,hijo.localPosition.z); 
+        maxScale = spring.localScale;
+        hijo.localPosition = (spring.localScale * -1);
        
     }
 
+    public IEnumerator repetirLerp(Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while( i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            spring.transform.localScale = Vector3.Lerp(a, b, i);
+            hijo.localPosition = (spring.localScale * -1);
+            yield return null;
+        }
+    }
 
 }
